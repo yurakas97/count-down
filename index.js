78a9b9c7233d1a6b6f96ad2376c4b2e9ab1522b4
yurakas97 <123570342+yurakas97@users.noreply.github.com>
2023-12-03 22:11:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 async function payForService() {
     const accounts = await web3.eth.getAccounts();
 
-    //checkNetwork()
+    checkNetwork()
 
     try {
         const result = await contract.methods.payService().send({
@@ -195,81 +195,81 @@ async function payForService() {
 }
 
 
-// // Отримайте інформацію про обрану мережу в MetaMask
-// async function getSelectedNetwork() {
-//     try {
-//         const networkId = await web3.eth.net.getId();
-//         return networkId;
-//     } catch (error) {
-//         console.error('Error getting network ID:', error.message);
-//         return null;
-//     }
-// }
+// Отримайте інформацію про обрану мережу в MetaMask
+async function getSelectedNetwork() {
+    try {
+        const networkId = await web3.eth.net.getId();
+        return networkId;
+    } catch (error) {
+        console.error('Error getting network ID:', error.message);
+        return null;
+    }
+}
 
-// // Перевірте, чи обрана мережа відповідає вашим вимогам
-// function checkNetwork() {
-//     const requiredNetworkId = 8073763; // ID потрібної мережі (1 для mainnet)
+// Перевірте, чи обрана мережа відповідає вашим вимогам
+function checkNetwork() {
+    const requiredNetworkId = 8073763; // ID потрібної мережі (1 для mainnet)
 
-//     // Спробуйте переключитись на потрібну мережу
-//     switchToRequiredNetwork(requiredNetworkId).then((success) => {
-//         if (!success) {
-//             // Якщо переключення не вдалося, спробуйте додати мережу
-//             addAndSwitchToNetwork(requiredNetworkId);
-//         }
-//     });
-// }
+    // Спробуйте переключитись на потрібну мережу
+    switchToRequiredNetwork(requiredNetworkId).then((success) => {
+        if (!success) {
+            // Якщо переключення не вдалося, спробуйте додати мережу
+            addAndSwitchToNetwork(requiredNetworkId);
+        }
+    });
+}
 
-// // Функція для переключення на задану мережу
-// function switchToRequiredNetwork(requiredNetworkId) {
-//     return new Promise((resolve) => {
-//         getSelectedNetwork().then((networkId) => {
-//             if (networkId !== null && networkId !== requiredNetworkId) {
-//                 // Запит на переключення мережі
-//                 window.ethereum.request({
-//                     method: 'wallet_switchEthereumChain',
-//                     params: [{ chainId: `0x${requiredNetworkId.toString(16)}` }],
-//                 }).then(() => {
-//                     console.log('Network switched successfully');
-//                     resolve(true);
-//                 }).catch((error) => {
-//                     console.error('Error switching network:', error.message);
-//                     resolve(false);
-//                 });
-//             } else {
-//                 // Вже на потрібній мережі
-//                 resolve(true);
-//             }
-//         });
-//     });
-// }
+// Функція для переключення на задану мережу
+function switchToRequiredNetwork(requiredNetworkId) {
+    return new Promise((resolve) => {
+        getSelectedNetwork().then((networkId) => {
+            if (networkId !== null && networkId !== requiredNetworkId) {
+                // Запит на переключення мережі
+                window.ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: `0x${requiredNetworkId.toString(16)}` }],
+                }).then(() => {
+                    console.log('Network switched successfully');
+                    resolve(true);
+                }).catch((error) => {
+                    console.error('Error switching network:', error.message);
+                    resolve(false);
+                });
+            } else {
+                // Вже на потрібній мережі
+                resolve(true);
+            }
+        });
+    });
+}
 
-// // Функція для додавання та переключення на задану мережу
-// function addAndSwitchToNetwork(requiredNetworkId) {
-//     // Отримайте інформацію про мережу для додавання та переключення
-//     const networkInfo = {
-//         chainId: `0x${requiredNetworkId.toString(16)}`, // Hex формат ID мережі
-//         chainName: 'yurakas_8073763-1',
-//         nativeCurrency: {
-//             name: 'KAS',
-//             symbol: 'KAS',
-//             decimals: 18,
-//         },
-//         rpcUrls: ['http://46.101.121.126:8545/'], // Замініть на свій Infura Project ID
-//         blockExplorerUrls: [''],
-//     };
+// Функція для додавання та переключення на задану мережу
+function addAndSwitchToNetwork(requiredNetworkId) {
+    // Отримайте інформацію про мережу для додавання та переключення
+    const networkInfo = {
+        chainId: `0x${requiredNetworkId.toString(16)}`, // Hex формат ID мережі
+        chainName: 'yurakas_8073763-1',
+        nativeCurrency: {
+            name: 'KAS',
+            symbol: 'KAS',
+            decimals: 18,
+        },
+        rpcUrls: ['http://46.101.121.126:8545/'], // Замініть на свій Infura Project ID
+        blockExplorerUrls: [''],
+    };
 
-//     // Додайте та переключіться на мережу
-//     window.ethereum.request({
-//         method: 'wallet_addEthereumChain',
-//         params: [networkInfo],
-//     }).then(() => {
-//         console.log('Network added successfully');
-//         // Переключення на додану мережу
-//         switchToRequiredNetwork(requiredNetworkId);
-//     }).catch((error) => {
-//         console.error('Error adding network:', error.message);
-//     });
-// }
+    // Додайте та переключіться на мережу
+    window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [networkInfo],
+    }).then(() => {
+        console.log('Network added successfully');
+        // Переключення на додану мережу
+        switchToRequiredNetwork(requiredNetworkId);
+    }).catch((error) => {
+        console.error('Error adding network:', error.message);
+    });
+}
 
-// // Викличте перевірку мережі
-// //checkNetwork();
+// Викличте перевірку мережі
+//checkNetwork();
